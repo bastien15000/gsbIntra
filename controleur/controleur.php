8a59bed4ac0dc8e23177628a5a2b25c225198a3c
Alexandre MOREIRA <amoreira@SIO63.local>
2020-12-01 17:04:17 +0100

    init

---
+++ controleur/controleur.php
@@ -0,0 +1,32 @@
+<?php
+
+function accueil()
+{
+	
+    require('./view/frontend/accueil.php');
+}
+
+function france()
+{
+    require('./view/frontend/france.php');
+}
+
+function connection()
+{
+    require('./view/frontend/connection.php');
+}
+
+function contact()
+{
+    require('./view/frontend/contact.php ');
+}
+
+function international()
+{
+    require('./view/frontend/international.php');
+}
+
+function produit()
+{
+    require('./view/frontend/produits.php');
+}
---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+require('controller/FrontController.php');
+
+$page = $_GET['page'] ?? null;
+switch ($page) {
+    case 'france': 
+        france();
+        break;
+    case 'contact':
+        contact();
+        break;
+    case 'international':
+        international();
+        break;
+    case 'produits' :
+        produit();
+        break;
+    case 'connection':
+        connection();
+        break;
+    default :
+        accueil();
+        break;
+}
